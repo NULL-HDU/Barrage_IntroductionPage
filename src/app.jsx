fd0b6908b9aaa21944f6b7b53623591d6e4af3e6
mpsss <mephistommm@gmail.com>
2016-10-20 21:58:28 +0800

    modify image format

--- src/app.jsx
+++ src/app.jsx
@@ -13,6 +13,10 @@ import ImageCard from "./component/image_card.jsx";
 import Content from "./component/content.jsx"
 import AppCss from "./app.css"
 
+import ButtonMin from "./src/img/Button-min.png"
+import MaybeAPicMin from "./src/img/MaybeAPic-min.png"
+import GitHubMin from "./src/img/GitHub-min.png"
+
 class App extends Component {
 	constructor(props){
 		super(props);
@@ -39,17 +43,17 @@ class App extends Component {
 							<p className={AppCss.intro}>fight with dazzing bullets</p>
 						</div>
 						<div style={{height:"60px",display:"flex",alignItems:"center"}}>
-							<img src="./src/img/Button-min.png" style={{marginRight:"40px"}} />
+							<img src={ButtonMin} style={{marginRight:"40px"}} />
 							<a href="#" style={{textDecoration:"none"}}><span style={{height:"60px",verticalAlign:"center",color:"blue",fontSize:"18px"}}>or for more information</span></a>
 						</div>					
 					</div>
-					<img src="./src/img/MaybeAPic-min.png" alt="img"/>
+					<img src={MaybeAPicMin} alt="img"/>
 				</Content>
 
 	 			<BothEnd style={{margin:"125px auto 0",height:"45px"}}>
 					<span>The NULL-HDU Team</span>
 					<span>Visit us at github</span>
-					<a href="#"><img src="./src/img/GitHub-min.png" /></a>
+					<a href="#"><img src={GitHubMin} /></a>
 				</BothEnd>
 			</div>
 		)
